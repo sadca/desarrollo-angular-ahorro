43f340d2246d711bffd1230cfe0b0665866ff85b
davidvalverdeoria <davidvalverde251293@hotmail.com>
2019-09-22 12:15:16 +0200

    Mejora cálculo imágen

--- src/app/components/calculo-imagen/calculo-imagen.component.ts
+++ src/app/components/calculo-imagen/calculo-imagen.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-calculo-imagen',
   templateUrl: './calculo-imagen.component.html'
 })
 export class CalculoImagenComponent implements OnInit {
+  email: string;
+  nombre: string;
+  archivo: any;
+
   constructor() {}
 
   ngOnInit() {}
+
+  login(form: NgForm) {
+    console.log(this.archivo);
+    console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    Swal.fire({
+      type: 'info',
+      text: 'Calculando...',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+    Swal.close();
+    Swal.fire({
+      type: 'success',
+      text: '¡¡Cargado!!'
+    });
+  }
 }
